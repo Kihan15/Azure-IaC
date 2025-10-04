d5478d47b9077291cee773808c2768a0847d7094
Kihan15 <test.bin75@gmail.com>
2025-10-04 23:54:37 +0200

    ok

--- main/terraform.tf
+++ main/terraform.tf
@@ -2,19 +2,19 @@
 # TERRAFORM CONFIG
 ##################################################################################
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "2.84.0"
-        }
-          random = {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    random = {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
-    }
-    backend "azurerm" {
-        key = "app.terraform.tfstate"
-    }
+  }
+  backend "azurerm" {
+    key = "app.terraform.tfstate"
+  }
 }
 
 
@@ -23,6 +23,5 @@ terraform {
 ##################################################################################
 
 provider "azurerm" {
-  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
